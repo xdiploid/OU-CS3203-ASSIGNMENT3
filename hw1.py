e231b6862a36e7d92c9f02e67b37c137d32cd144
xdiploid <artificialcheeto@gmail.com>
2023-02-14 23:38:54 -0600

    This is the second commit with the function that multiplies elements in a list and returns the value.

--- hw1.py
+++ hw1.py
@@ -5,3 +5,8 @@ def sum(list):
         sum = sum + list[idx]
     return sum
 
+def mult(list):
+    product = 1
+    for idx in range(len(list)):
+        product = product * list[idx]
+    return product
