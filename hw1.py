b782df6003a08f48e0f832df3befd93c1cbeec2f
xdiploid <artificialcheeto@gmail.com>
2023-02-14 23:40:35 -0600

    This is the third commit with the main function that takes user input, creates an array, and returns the sum and product of all elements.

--- hw1.py
+++ hw1.py
@@ -10,3 +10,13 @@ def mult(list):
     for idx in range(len(list)):
         product = product * list[idx]
     return product
+
+def main():
+    size = int(input("Enter the size of your list: "))
+    list = []
+    for idx in range(size):
+        list.append(int(input("Enter an integer to add to your list: ")))
+    sum_of_list = sum(list)
+    product_of_list = mult(list)
+    print(f'The sum of elements is {sum_of_list}')
+    print(f'The product of elements is {product_of_list}')
