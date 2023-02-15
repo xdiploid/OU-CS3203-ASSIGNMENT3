49da734270e6c4729247e944253f16c60d26a53a
xdiploid <artificialcheeto@gmail.com>
2023-02-14 23:48:05 -0600

    Added reverse function to main method.

--- hw1.py
+++ hw1.py
@@ -22,7 +22,10 @@ def main():
     list = []
     for idx in range(size):
         list.append(int(input("Enter an integer to add to your list: ")))
+        
     sum_of_list = sum(list)
     product_of_list = mult(list)
+    reversed_list = reverse(list)
     print(f'The sum of elements is {sum_of_list}')
     print(f'The product of elements is {product_of_list}')
+    print(f'The reversed list is {reversed_list}')
