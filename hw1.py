46c4097dd67252001a0fb3d2c4fca42edbc1e747
xdiploid <artificialcheeto@gmail.com>
2023-02-15 19:42:36 -0600

    Final commit.

--- hw1.py
+++ hw1.py
@@ -1,10 +1,3 @@
-
-def sum(list):
-    sum = 0
-    for idx in range(len(list)):
-        sum = sum + list[idx]
-    return sum
-
 def mult(list):
     product = 1
     for idx in range(len(list)):
@@ -22,7 +15,11 @@ def main():
     list = []
     for idx in range(size):
         list.append(int(input("Enter an integer to add to your list: ")))
+<<<<<<< HEAD
         
+=======
+
+>>>>>>> 135d07a (Changed comments above the main method function call.)
     sum_of_list = sum(list)
     product_of_list = mult(list)
     reversed_list = reverse(list)
@@ -30,6 +27,11 @@ def main():
     print(f'The product of elements is {product_of_list}')
     print(f'The reversed list is {reversed_list}')
 
+<<<<<<< HEAD
 ## The main function will now run and prompt the user to provide information. Afterwards, all of the functions will be
 ## and the outputs will be printed to the user.
+=======
+
+## The main() method call will prompt users to enter information and will return print statements to the user.
+>>>>>>> 135d07a (Changed comments above the main method function call.)
 main()
